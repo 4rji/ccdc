04dfba7fdf39c6cdfde58749be019f116a0defb7
jav-jim-mn <118249932+jav-jim-mn@users.noreply.github.com>
2023-03-02 04:29:39 -0600

    Delete NagCentos directory

--- NagCentos/2plug.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/nagios-plugins/nagios-plugins/releases/download/release-2.4.3/nagios-plugins-2.4.3.tar.gz
-tar -xzvf nagios-plugins-2.4.3.tar.gz 
-cd nagios-plugins-2.4.3/
-./configure
-make install
-systemctl restart nagios
-systemctl status nagios
--- NagCentos/interval.sh
+++
@@ -1,7 +0,0 @@
-
-#!/bin/bash
-
-cp /usr/local/nagios/etc/objects/templates.cfg /usr/local/nagios/etc/objects/.templates.cfg.backup 
-nano /usr/local/nagios/etc/objects/templates.cfg
-
-mv templates.cfg /usr/local/nagios/etc/objects/
